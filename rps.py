7ef39e063b62ea6872c37a5144c96bbfcd535314
Chuck South <cs938w@att.com>
2021-01-08 13:02:58 -0500

    pycodestyle edits

--- rps.py
+++ rps.py
@@ -19,13 +19,15 @@ class Player:
     def learn(self, my_move, their_move):
         pass
 
+
 class RandomPlayer:
     def move(self):
         return random.choice(moves)
-    
+
     def learn(self, my_move, their_move):
         self.my_move = my_move
         self.their_move = their_move
+
 
 class HumanPlayer:
     def move(self):
@@ -34,10 +36,11 @@ class HumanPlayer:
             move = s.lower()
             if move == 'rock' or move == 'paper' or move == 'scissors':
                 return move
-    
+
     def learn(self, my_move, their_move):
         pass
- 
+
+
 class ReflectPlayer:
     def move(self):
         return random.choice(moves)
@@ -46,11 +49,12 @@ class ReflectPlayer:
         self.my_move = my_move
         self.their_move = their_move
         return their_move
-        
+
+
 class CyclePlayer:
     def move(self):
         return random.choice(moves)
-    
+
     def learn(self, my_move, their_move):
         self.my_move = my_move
         self.their_move = their_move
@@ -61,55 +65,54 @@ class CyclePlayer:
         elif my_move == 'scissors':
             return 'rock'
 
+
 def beats(one, two):
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
             (one == 'paper' and two == 'rock'))
+
 
 class Game:
 
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        self.p1score = 0
-        self.p2score = 0
+        self.p1s = 0
+        self.p2s = 0
         self.turn = 0
-      
+
     def play_round(self):
         if self.turn == 0:
             move1 = self.p1.move()
-            move2 = self.p2.move() 
+            move2 = self.p2.move()
             print(f"Opponent played: {move1}\nYou played: {move2}")
             self.returned_move = self.p1.learn(move1, move2)
-            # self.p2.learn(move2, move1)
             self.turn += 1
         else:
             move1 = self.returned_move
             move2 = self.p2.move()
             print(f"Opponent played: {move1}\nYou played: {move2}")
             self.returned_move = self.p1.learn(move1, move2)
-            # self.p2.learn(move2, move1)
             self.turn += 1
         if beats(move1, move2):
-            self.p1score += 1     
+            self.p1s += 1
             print("**PLAYER 1 WINS THE ROUND**")
         elif beats(move2, move1):
-            self.p2score += 1
+            self.p2s += 1
             print("**PLAYER 2 WINS THE ROUND**")
         else:
             print("**TIE**")
-        print(f"The score is Player 1: {self.p1score}, Player 2: {self.p2score}.\n")
-
+        print(f"The score is Player 1: {self.p1s}, Player 2: {self.p2s}.\n")
 
     def play_game(self):
         print("Let's play Rock, Paper, Scissors!\n")
         for round in range(100):
             print(f"Round {round + 1}:")
             self.play_round()
-            if self.p1score > self.p2score + 2:
+            if self.p1s > self.p2s + 2:
                 print("**PLAYER 1 WINS THE GAME!**")
                 break
-            if self.p2score > self.p1score + 2:
+            if self.p2s > self.p1s + 2:
                 print("**PLAYER 2 WINS THE GAME!**")
                 break
         print("Game over!")
