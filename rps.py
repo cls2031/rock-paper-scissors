172a4795d84a0df7d3de460a0c2f7515736a7be1
Chuck South <cs938w@att.com>
2021-01-13 17:20:51 -0500

    added quit

--- rps.py
+++ rps.py
@@ -7,6 +7,7 @@ import random
 
 moves = ['rock', 'paper', 'scissors']
 
+rnd = 0
 
 """The Player class is the parent class for all of the Players
 in this game"""
@@ -20,50 +21,58 @@ class Player:
         pass
 
 
-class RandomPlayer:
+class RandomPlayer(Player):
     def move(self):
         return random.choice(moves)
 
     def learn(self, my_move, their_move):
-        self.my_move = my_move
-        self.their_move = their_move
+        pass
 
 
-class HumanPlayer:
+class HumanPlayer(Player):
     def move(self):
         while True:
             s = input("Rock, Paper, Scissors? ")
             move = s.lower()
             if move == 'rock' or move == 'paper' or move == 'scissors':
                 return move
-
-    def learn(self, my_move, their_move):
-        pass
-
-
-class ReflectPlayer:
-    def move(self):
-        return random.choice(moves)
+            if move == 'quit':
+                return move
 
     def learn(self, my_move, their_move):
         self.my_move = my_move
         self.their_move = their_move
-        return their_move
+        return self.my_move
 
 
-class CyclePlayer:
+class ReflectPlayer(Player):
     def move(self):
-        return random.choice(moves)
+        if rnd == 0:
+            return random.choice(moves)
+        else:
+            return self.their_move
 
     def learn(self, my_move, their_move):
         self.my_move = my_move
         self.their_move = their_move
-        if my_move == 'rock':
-            return 'paper'
-        elif my_move == 'paper':
-            return 'scissors'
-        elif my_move == 'scissors':
-            return 'rock'
+
+
+class CyclePlayer(Player):
+    def move(self):
+        if rnd == 0:
+            return random.choice(moves)
+        else:
+            if self.my_move == 'rock':
+                return 'paper'
+            elif self.my_move == 'paper':
+                return 'scissors'
+            elif self.my_move == 'scissors':
+                return 'rock'
+
+    def learn(self, my_move, their_move):
+        self.my_move = my_move
+        self.their_move = their_move
+        
 
 
 def beats(one, two):
@@ -79,22 +88,18 @@ class Game:
         self.p2 = p2
         self.p1s = 0
         self.p2s = 0
-        self.turn = 0
 
     def play_round(self):
-        if self.turn == 0:
-            move1 = self.p1.move()
-            move2 = self.p2.move()
-            print(f"Opponent played: {move1}\nYou played: {move2}")
-            self.returned_move = self.p1.learn(move1, move2)
-            self.turn += 1
-        else:
-            move1 = self.returned_move
-            move2 = self.p2.move()
-            print(f"Opponent played: {move1}\nYou played: {move2}")
-            self.returned_move = self.p1.learn(move1, move2)
-            self.turn += 1
-        if beats(move1, move2):
+        move1 = self.p1.move()
+        move2 = self.p2.move()
+        print(f"Player 1 played: {move1}\nPlayer 2 played: {move2}")
+        self.p1_returned_move = self.p1.learn(move1, move2)
+        self.p2_returned_move = self.p2.learn(move2, move1)
+        global rnd
+        rnd += 1
+        if self.p1_returned_move == "quit" or self.p2_returned_move == "quit":
+            return 
+        elif beats(move1, move2):
             self.p1s += 1
             print("**PLAYER 1 WINS THE ROUND**")
         elif beats(move2, move1):
@@ -109,6 +114,9 @@ class Game:
         for round in range(100):
             print(f"Round {round + 1}:")
             self.play_round()
+            if self.p1_returned_move == "quit" or self.p2_returned_move == "quit":
+                print("**PLAYER QUITS!**")
+                break            
             if self.p1s > self.p2s + 2:
                 print("**PLAYER 1 WINS THE GAME!**")
                 break
@@ -119,5 +127,5 @@ class Game:
 
 
 if __name__ == '__main__':
-    game = Game(CyclePlayer(), HumanPlayer())
+    game = Game(RandomPlayer(), HumanPlayer())
     game.play_game()
